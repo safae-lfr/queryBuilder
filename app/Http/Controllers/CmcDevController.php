8491b8b95e70d693ee1981530a2e52e5e29dc765
EL FARCHOUNI SAFAE <SAFAE.EL FARCHOUNI@CMCTTA.MA>
2026-02-27 09:22:17 +0000

    model

---
+++ app/Http/Controllers/CmcDevController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CmcDev;
+use Illuminate\Http\Request;
+
+class CmcDevController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CmcDev $cmcDev)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CmcDev $cmcDev)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CmcDev $cmcDev)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CmcDev $cmcDev)
+    {
+        //
+    }
+}
